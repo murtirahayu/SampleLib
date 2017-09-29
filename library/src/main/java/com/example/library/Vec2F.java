683c94822ca97a3ed3893a6d8e934170282ed4a3
murtirahayu3@gmail.com <murtirahayu3@gmail.com>
2017-09-29 17:05:20 +0700

    commit pertama

---
+++ library/src/main/java/com/example/library/Vec2F.java
@@ -0,0 +1,29 @@
+package com.example.library;
+
+/**
+ * Created by fasapay on 9/29/17.
+ */
+
+public class Vec2F {
+
+    public float x, y;
+
+    public Vec2F(Vec2F vec){
+        this.x = vec.x;
+        this.y = vec.y;
+    }
+
+    public Vec2F(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vec2F copy(){
+        return new Vec2F(this);
+    }
+
+    public void debug(){
+        System.out.println("{"+x+", "+y+"}");
+    }
+
+}
